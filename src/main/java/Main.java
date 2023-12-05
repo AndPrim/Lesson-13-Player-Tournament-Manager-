42599ee4089dab43be1b36925ba11d93911ca0a0
AndPrim <andreyminoss@gmail.com>
2023-12-05 15:38:45 +0500

    OneCommit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import ru.netology.playerManager.Player;
+
+public class Main {
+    public static void main(String[] args) {
+        Player service = new Player();
+    }
+}
---
+++ src/main/java/ru/netology/playerManager/Game.java
@@ -0,0 +1,5 @@
+package ru.netology.playerManager;
+
+public class Game {
+    Player player = new Player();
+}
